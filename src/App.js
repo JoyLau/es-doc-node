9d80b4b28e1ae2936de94810d183038a284f3681
liufa <2587038142>
2018-06-11 22:24:15 +0800

    update 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
-import searchData from "../data/json/search.json";
 import {Card, Input} from "antd";
 import $ from "jquery";
 const Search = Input.Search;
-
-const API = "http://192.168.3.74:9200";
+const API = "http://192.168.10.74:9200/idioms-dictionary/_search";
 
 class App extends Component {
 
     state={
         data:[],
+        searchData : {}
     };
+    componentWillMount() {
+    }
+
+    componentDidMount() {
+        let that = this;
+        $.getJSON("../data/json/search.json",function (data) {
+            that.setState({
+                searchData : data
+            });
+        })
+    }
 
     getData(value){
+        let that = this;
         if (value.toString().trim()) {
-            $.getJSON(API + "/ik-index/_search",searchData,function (data) {
-                console.info(data)
-            })
+            let searchData = this.state.searchData;
+            searchData.query.bool.should[0].match.chengyu = value;
+            searchData.query.bool.should[1].match.chuchu = value;
+            searchData.query.bool.should[2].match.diangu = value;
+            searchData.query.bool.should[3].match.lizi = value;
+            searchData.query.bool.should[4].match.spinyin = value;
+            $.ajax({
+                url: API,
+                type: 'POST',
+                contentType: 'application/json; charset=UTF-8',
+                dataType: 'json',
+                data: JSON.stringify(searchData),
+                success: function(res, status, xhr) {
+                    let data = [];
+                    res.hits.hits.map((item)=>{
+                        let obj = item._source;
+                        if (item.highlight.chengyu) {
+                            obj.chengyu = item.highlight.chengyu
+                        }
+                        if (item.highlight.chuchu) {
+                            obj.chuchu = item.highlight.chuchu
+                        }
+                        if (item.highlight.diangu) {
+                            obj.diangu = item.highlight.diangu
+                        }
+                        if (item.highlight.lizi) {
+                            obj.lizi = item.highlight.lizi
+                        }
+                        if (item.highlight.spinyin) {
+                            obj.spinyin = item.highlight.spinyin
+                        }
+
+                        data.push(obj)
+                    });
+
+                    that.setState({
+                        data:data,
+                    })
+                }
+            });
         }
     }
 
@@ -29,16 +77,26 @@ class App extends Component {
                   placeholder="Search Subject"
                   enterButton="Search"
                   size="large"
-                  onSearch={value => console.log(value)}
+                  onSearch={value => this.getData(value)}
                   onInput={event => this.getData(event.target.value)}
               />
           </div>
           <div style={{margin: '30px auto',width:'40%'}}>
-              <Card title="Card title">
-                  <p>Card content</p>
-                  <p>Card content</p>
-                  <p>Card content</p>
-              </Card>
+              {
+                  this.state.data.map((item,index)=>{
+                      return (
+                          <Card title={<span>{item.chengyu}  【{item.pinyin}】</span>}
+                                style={{marginTop:30}}
+                                key = {index}
+                                hoverable = {true}
+                          >
+                              <p>出处： {item.chuchu}</p>
+                              <p>典故： {item.diangu}</p>
+                              <p>列子:  {item.lizi}</p>
+                          </Card>
+                      )
+                  })
+              }
           </div>
       </div>
     );
